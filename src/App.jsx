88da305bb9e6b38759c83ba3332070080c77ce34
shinthantlwin <shinshinshinstl@gmail.com>
2025-05-13 14:15:44 +0800

    add contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import About from './components/About'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
     <div>
       <About />
+      <Contact />
     </div>
   )
 }
